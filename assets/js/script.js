eaad6c054347812a457815104b2c8c3de0455d1b
nexeption <nexeption@gmail.com>
2025-08-09 22:53:10 +0300

    ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,6 @@ import { initModal } from './scripts/modal.js';
 import { initNewTab } from './scripts/new_tab.js';
 import { initCopy } from './scripts/copy.js';
 import { initCopySmall } from './scripts/copy_small.js';
+import { preloadFonts } from './scripts/preload_font.js';
 
-(initModal(), initNewTab(), initCopy(), initCopySmall());
+(initModal(), initNewTab(), initCopy(), initCopySmall(), preloadFonts());
