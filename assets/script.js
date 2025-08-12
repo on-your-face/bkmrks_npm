74e9f1d679f30474222f7b8ff45712d01ce59a88
nexeption <161155305+on-your-face@users.noreply.github.com>
2025-08-12 15:48:33 +0300

    ok

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,17 @@
+// scss
 import './sass/main/reset.scss';
 import './sass/main/base.scss';
 import './sass/main/fonts.scss';
 import './sass/main/main.scss';
 import './sass/modal_container.scss';
-
-
 import './sass/style.scss';
-import './sass/modal_gyv.scss';
-import './sass/divider.scss';
+
+import './sass/other/divider.scss';
+// scripts
 import { initModal } from './js/modal.js';
 import { initNewTab } from './js/new_tab.js';
 import { initCopy } from './js/copy.js';
 import { initCopySmall } from './js/copy_small.js';
 import { preloadFonts } from './js/preload_font.js';
-
+// initScript
 (initModal(), initNewTab(), initCopy(), initCopySmall(), preloadFonts());
