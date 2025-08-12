6984573fbc7d8905f95aa353a8514c82a63206d4
nexeption <161155305+on-your-face@users.noreply.github.com>
2025-08-12 15:46:03 +0300

    ok

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,8 @@
-import './sass/reset.scss';
-import './sass/base.scss';
-import './sass/fonts.scss';
-import './sass/main.scss';
-
-
+import './sass/main/reset.scss';
+import './sass/main/base.scss';
+import './sass/main/fonts.scss';
+import './sass/main/main.scss';
+import './sass/modal_container.scss';
 
 
 import './sass/style.scss';
