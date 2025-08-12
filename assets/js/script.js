4d0c723aecbb9073c9f9b1ae515abf39d20a550d
nexeption <161155305+on-your-face@users.noreply.github.com>
2025-08-12 13:54:19 +0300

    ok

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-import { initModal } from './scripts/modal.js';
-import { initNewTab } from './scripts/new_tab.js';
-import { initCopy } from './scripts/copy.js';
-import { initCopySmall } from './scripts/copy_small.js';
-import { preloadFonts } from './scripts/preload_font.js';
-
-(initModal(), initNewTab(), initCopy(), initCopySmall(), preloadFonts());
---
+++ assets/script.js
@@ -0,0 +1,13 @@
+import './sass/reset.scss';
+import './sass/main.scss';
+import './sass/fonts.scss';
+import './sass/style.scss';
+import './sass/element.scss';
+import './sass/modal_gyv.scss';
+import { initModal } from './js/modal.js';
+import { initNewTab } from './js/new_tab.js';
+import { initCopy } from './js/copy.js';
+import { initCopySmall } from './js/copy_small.js';
+import { preloadFonts } from './js/preload_font.js';
+
+(initModal(), initNewTab(), initCopy(), initCopySmall(), preloadFonts());
